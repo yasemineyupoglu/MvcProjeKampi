bd6bb7646172257deed84dda228445a99fc9e371
yasemin <yasemin.eyupoglu.98@gmail.com>
2021-04-28 16:01:50 +0300

    tekrar

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        //CRYD
+        //Type Name();
+        List<Category> List();
+
+        void Insert(Category p);
+
+        void Update(Category p);
+
+        void Delete(Category p);
+
+        List<Category> List(Expression<Func<Category, bool>> filter);
+    }
+}
